da2b5fa2477199cef98571f3eb4e713bbc7cd01f
C. Hamilton <cidney@cidney.org>
2014-12-11 23:42:20 -0500

    Add everything from TwentyEleven to start work on new theme.

---
+++ sidebar.php
@@ -0,0 +1,23 @@
+
+<div id="secondary" class="widget-area" role="complementary">
+
+	<aside id="search-2" class="widget widget_search">
+	  <form method="get" id="searchform" action="/">
+	    <label for="s" class="assistive-text">Search</label>
+	    <input class="field" name="s" id="s" placeholder="Search" type="text">
+	    <input class="submit" name="submit" id="searchsubmit" value="Search" type="submit">
+	  </form>
+	</aside>
+	<aside class="widget">
+	  <a href="/tickets" target="_self" class="tickets-link" title="Buy Tickets Now">
+	  </a>
+	</aside>		
+	<aside class="widget">
+	  <a href="/catering" target="_self" class="drinks-link" title="Drinks and Dining">
+	  </a>
+	</aside>	
+	
+	<aside class="widget">
+	  <div class="fb-like" data-href="https://www.facebook.com/WickedFaire" data-width="120" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+	</aside>
+</div><!-- #secondary .widget-area -->
