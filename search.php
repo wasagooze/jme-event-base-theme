0779890e3f3bf4b5161314afd972ece9ebc97556
C. Hamilton <cidney@cidney.org>
2015-01-05 14:32:55 -0500

    Fix search code.

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Template for displaying Search Results pages
- *
  */
 
 get_header(); ?>
@@ -12,10 +11,8 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyeleven' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'jme-event-base-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header>
-
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
 
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -32,17 +29,15 @@ get_header(); ?>
 
 				<?php endwhile; ?>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
 			<?php else : ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyeleven' ); ?></p>
+						<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
