bf3df5f590a1e595c9c03e624f81d199b23eac6a
C. Hamilton <cidney@cidney.org>
2015-02-26 14:27:45 -0500

    New layout for attractions.

--- category.php
+++ category.php
@@ -1,57 +1,56 @@
 <?php
 /**
- * Template for displaying Category Archive pages
+ * Template for displaying Attraction Archive pages
  */
+
+$category_name = get_category(get_query_var('cat'))->slug;
+
+$args = array( 'post_type' => 'attraction', 'category_name' => $category_name);
+
+$query = new WP_Query( $args );
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+    <section id="primary">
+      <div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+      <?php if ( $query->have_posts() ) : ?>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php echo single_cat_title( '', false ); ?></h1>
+        <header class="page-header">
+          <h1 class="page-title"><?php echo single_cat_title( '', false ); ?></h1>
 
-					<?php
-						$category_description = category_description();
-						if ( ! empty( $category_description ) ) {
-							echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
+          <?php
+            $category_description = category_description();
+            if ( ! empty( $category_description ) ) {
+              echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
+            }
+          ?>
+        </header>
+
+        <ul class="category-listing">
+        <?php /* Start the Loop */ ?>
+        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+       	 	<a href="<?php the_permalink(); ?>">
+       	 	<?php 
+						if ( has_post_thumbnail() ) {
+							$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
 						}
-					?>
-				</header>
+					?>	
+          <li class="attraction-thumbnail" style="background-image:url('<?php echo $large_image_url[0]; ?>');">
+          	<?php the_title(); ?>          	
+          </li>
+          </a>
+        <?php endwhile; ?>
+        </li>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+      <?php else : ?>
 
-					<?php
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+        <?php include('404-content.php'); ?>
 
-				<?php endwhile; ?>
+      <?php endif; ?>
 
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title">Nothing Found</h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
+      </div><!-- #content -->
+    </section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- content-attraction.php
+++ content-attraction.php
@@ -1,6 +1,6 @@
 <?php 
 
-  /* Template for displaying Attractions within Category listing */
+  /* Template for displaying single-page attractions */
 
   if ( has_post_thumbnail() ) {
     $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
@@ -21,9 +21,31 @@
   $reverb = get_post_meta($id, 'attraction_reverbnation', true);
   $video = get_post_meta( $id, 'attraction_video', true);
 ?>
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="entry-header">
+      <h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    </header><!-- .entry-header -->
+
+    <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+      <div class="entry-summary">
+        <?php the_excerpt(); ?>
+      </div><!-- .entry-summary -->
+      <?php else : ?>
+      <div class="entry-content">
+
         <section class="attraction">
-      
-          <ul class="links">
+    
+          <a href="<?php echo $website; ?>"><?php the_post_thumbnail('large', array('class' => 'alignright')); ?></a>
+
+          <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'jme-event-base-theme' ) ); ?>
+
+          <?php if (is_single() && $video !== ''): ?>
+            <iframe src='<?php echo $video; ?>' frameborder='0' allowfullscreen='allowfullscreen'></iframe>
+          <?php endif; ?>
+         
+          <ul class="social">
             <?php if ( $website != '') : ?>
               <li><a href="<?php echo $website; ?>" class="web" rel="me" target="_blank">Website</a></li>
             <?php endif; ?>
@@ -46,23 +68,13 @@
               <li><a href="<?php echo $reverb; ?>" class="reverb" rel="me" target="_blank">ReverbNation</a></li>
             <?php endif; ?>
          </ul>
-
-          <a href="<?php echo $website; ?>"><?php the_post_thumbnail('medium', array('class' => 'alignright')); ?></a>
-
-          <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'jme-event-base-theme' ) ); ?>
-
-          <?php if (is_single() && $video !== ''): ?>
-            <iframe src='<?php echo $video; ?>' frameborder='0' allowfullscreen='allowfullscreen'></iframe>
-          <?php endif; ?>
-         
         </section>
 
-<?php /*
-<a href="<?php the_permalink(); ?>">
-<li class="attraction-thumbnail" 
-    style="background-image:url('<?php echo $large_image_url[0]; ?>');"> 
-  <?php the_title(); ?>           
-</li>
-</a>
 
-*/ ?>
+    </div><!-- .entry-content -->
+    <?php endif; ?>
+
+    <footer class="entry-meta">
+      <?php edit_post_link( __( 'Edit', 'jme-event-base-theme' ), '<span class="edit-link">', '</span>' ); ?>
+    </footer><!-- .entry-meta -->
+  </article><!-- #post-<?php the_ID(); ?> -->
