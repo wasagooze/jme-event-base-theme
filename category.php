4654ea2b5b1ec36fbea6c4c0d86d5a10cc69a19b
C. Hamilton <cidney@cidney.org>
2015-02-27 20:25:06 -0500

    Fix bug in featured image loop.

--- category.php
+++ category.php
@@ -32,9 +32,10 @@ get_header(); ?>
         <?php while ( $query->have_posts() ) : $query->the_post(); ?>
        	 	<a href="<?php the_permalink(); ?>">
        	 	<?php 
+            $large_image_url = '';
 						if ( has_post_thumbnail() ) {
 							$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0];
-						}
+						} 
 					?>	
           <li class="attraction-thumbnail" style="background-image:url('<?php echo $large_image_url; ?>')">
           	<div class="attraction-title"><?php the_title(); ?></div>
