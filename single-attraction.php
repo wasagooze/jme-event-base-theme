ebe4883e1a3f934a324cdde1ae9f6388d69af1d3
cidneyhamilton <chamilton@alum.wellesley.edu>
2015-07-03 14:15:00 -0400

    Upate single attraction.

--- single-attraction.php
+++ single-attraction.php
@@ -11,20 +11,6 @@ get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<header class="page-header">
-	<?php $category = get_top_category(get_the_category(get_the_ID())); 
-	if ($category !== null): ?>
-  	<h1 class="page-title"><?php echo $category->cat_name ?></h1>
-
-  <?php $cat_id = $category->cat_ID; ?>
-
-  <div class="attraction-filters">
-  <?php echo jme_get_attraction_index($cat_id); ?>
-  </div>
-<?php endif; ?>
- 
-</header>
-
           <?php get_template_part( 'content-attraction', get_post_format() ); ?>
 
         <?php endwhile; // end of the loop. ?>
