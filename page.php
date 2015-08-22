ca9a67980191b727d0b82a8da3f6cba0fc39047c
cidneyhamilton <chamilton@alum.wellesley.edu>
2015-08-22 14:01:15 -0400

    Page and single template bases.

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Sidebar Template
+ *
+ * Description: A Page Template that adds a sidebar to pages.
+ *
+ */
+
+get_header(); ?>
+
+<section id="primary">
+	<div id="content" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( '_partials/content', 'page' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+	</div><!-- #content -->
+</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template for displaying all single posts
+ */
+
+get_header(); 
+
+$post_type = get_post_type();
+
+?>
+
+<section id="primary">
+	<div id="content" role="main">			
+
+	<?php get_template_part('_partials/pageheader'); ?>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( '_partials/content', $post_type); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+	</div><!-- #content -->
+</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
