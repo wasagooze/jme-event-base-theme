6c35b109d19ab307f2b5074573b2dcdfe9d352bc
cidneyhamilton <chamilton@alum.wellesley.edu>
2015-09-06 10:49:31 -0400

    Compare empty author description to the empty string, not PHP empty.

--- _partials/pageheader-post.php
+++ _partials/pageheader-post.php
@@ -1,12 +1,14 @@
+<?php
+$description = get_the_author_meta('description');
+?>
+
 <header class="entry-header">
 	<h1 class="entry-title">
 		<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 	</h1>
-	<?php if (is_category('blog') || in_category('blog')): ?>
-		<?php if (!empty(get_the_author_meta('description'))): ?>
+	<?php if (is_category('blog') || in_category('blog') && $description != ""): ?>
 		<h2 class="byline">
 		by <b><?php the_author_posts_link(); ?></b> on <b><?php echo the_time(get_option('date_format')); ?></b>
 		</h2>
-		<?php endif; ?>
 	<?php endif; ?>
 </header><!-- .entry-header -->
